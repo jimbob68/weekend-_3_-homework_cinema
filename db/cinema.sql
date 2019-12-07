68a4e398678bc8b2670f60dc4897ed35e49ec082
James Fraser <jimbob4t@virginmedia.com>
2019-12-07 13:11:59 +0000

    update method working

--- db/cinema.sql
+++ db/cinema.sql
@@ -2,11 +2,7 @@ DROP TABLE tickets;
 DROP TABLE films;
 DROP TABLE customers;
 
-CREATE TABLE tickets (
-id SERIAL PRIMARY KEY,
-customers_id INT REFERENCES customers(id) ON DELETE CASCADE,
-film_id INT REFERENCES films(id) ON DELETE CASCADE
-);
+
 
 CREATE TABLE films (
 id SERIAL PRIMARY KEY,
@@ -18,4 +14,10 @@ CREATE TABLE customers (
 id SERIAL PRIMARY KEY,
 name VARCHAR(255),
 funds INT
+);
+
+CREATE TABLE tickets (
+id SERIAL PRIMARY KEY,
+customers_id INT REFERENCES customers(id) ON DELETE CASCADE,
+film_id INT REFERENCES films(id) ON DELETE CASCADE
 );
--- models/films.rb
+++ models/films.rb
@@ -18,11 +18,11 @@ class Film
     @id = film['id'].to_i
   end
 
-  # def update()
-  #   sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
-  #   values = [@title, @price, @id]
-  #   SqlRunner.run(sql, values)
-  # end
+  def update()
+    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
 
   # def delete()
   #   sql = "DELETE FROM films WHERE id = $1"
